282c940129b10bedb8aee3e549a0121ffc1d7b36
Fenitriniaina <fandrianarisata2@gmail.com>
2021-04-15 17:03:48 +0300

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{   
+    
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+    /**
+     * @Route("/", name="first")
+     */
+    public function first():RedirectResponse
+    {   
+        return $this->redirectToRoute("app_login");
+    }
+
+    /**
+     * @Route("/chat", name="chat")
+     */
+    public function chat():Response
+    {   
+        return $this->render("chat/index.html.twig");
+    }
+
+    /**
+     * @Route("/user_register", name="user_register")
+     */
+    public function user_register():Response
+    {   
+        return $this->render("security/user_register.html.twig");
+    }
+}
